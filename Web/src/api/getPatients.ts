68879f3b9df9e0fb85fdb877fa6d72438fec24fa
yanir <yanir.laty@gmail.com>
2026-01-24 22:30:25 +0200

    added web side API for create new user | cleaned up types to dedicated folder

--- Web/src/api/getPatients.ts
+++ Web/src/api/getPatients.ts
@@ -1,39 +1,12 @@
 import apiClient from "./axiosClient";
-
-export interface Patient {
-  patient_id: string;
-  user_id: string;
-  first_name: string;
-  last_name: string;
-  date_of_birth: string;
-  gender: string;
-  national_id_number: string;
-  phone_number: string;
-  users?: { email?: string };
-  address: string;
-  blood_type: string;
-  emergency_contact: string;
-  emergency_contact_phone: string;
-  insurance_provider: string;
-  insurance_policy_number: string;
-}
-
-export interface patientFilterTypes {
-  patient_id?: string;
-  user_id?: string;
-  national_id_number?: string;
-  first_name?: string;
-  last_name?: string;
-  phone_number?: string;
-}
+import type { Patient, patientFilterTypes } from "./types/patients";
 
 //this function fetches all users with a role of 'patient' from the backend API
 export const getPatients = async (
   filter?: patientFilterTypes,
 ): Promise<Patient[]> => {
   try {
-    const baseUrl = import.meta.env.VITE_API_URL;
-    const res = await apiClient.get<Patient[]>(`${baseUrl}/patients`, {
+    const res = await apiClient.get<Patient[]>(`patients`, {
       params: filter,
     });
     console.log("getPatients success:", res.data.length, "items found");
---
+++ Web/src/api/types/patients.ts
@@ -0,0 +1,26 @@
+export interface Patient {
+  patient_id: string;
+  user_id: string;
+  first_name: string;
+  last_name: string;
+  date_of_birth: string;
+  gender: string;
+  national_id_number: string;
+  phone_number: string;
+  users?: { email?: string };
+  address: string;
+  blood_type: string;
+  emergency_contact: string;
+  emergency_contact_phone: string;
+  insurance_provider: string;
+  insurance_policy_number: string;
+}
+
+export interface patientFilterTypes {
+  patient_id?: string;
+  user_id?: string;
+  national_id_number?: string;
+  first_name?: string;
+  last_name?: string;
+  phone_number?: string;
+}
