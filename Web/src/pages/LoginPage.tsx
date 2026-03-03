ec3cbf81ab463c5334fa7d0b79cb28f993896aee
נדב מייק <nadavmeik@Nadavs-MacBook-Pro.local>
2026-03-03 15:22:02 +0200

    chore: remove hebrew comments from login page

--- Web/src/pages/LoginPage.tsx
+++ Web/src/pages/LoginPage.tsx
@@ -3,7 +3,6 @@ import { Login } from "../components/Login";
 export const LoginPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-50 relative overflow-hidden">
-      {/* רקעים עדינים ובהירים שנותנים תחושה נקייה */}
       <div className="absolute top-[-10%] left-[-10%] w-[40%] h-[40%] bg-indigo-100/50 rounded-full blur-[100px]" />
       <div className="absolute bottom-[-10%] right-[-10%] w-[40%] h-[40%] bg-blue-100/50 rounded-full blur-[100px]" />
 
